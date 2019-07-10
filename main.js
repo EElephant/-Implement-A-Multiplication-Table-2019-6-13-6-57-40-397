150f9271b82d9b5a5fc8393cd64987b75cda02bc
Ethan <ethan.huang@oocl.com>
2019-07-10 11:02:47 +0800

    standardize Naming

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-function isStartLessThanEnd (start,end){
+const isStartLessThanEnd = (start,end) =>{
 	return start<=end;
 }
 
-function isInRange(start,end){
+const isInRange = (start,end) => {
 	return (start<=1000&&start>=1) && (end<=1000&&end >=1);
 }
 
-function createMultiplyTable(start,end){
+const createMultiplyTable = (start,end) => {
 	if(isStartLessThanEnd(start,end) == false){
 		return null;
 	}else if(isInRange(start,end) == false){
 		return 'out of range';
 	}else{
 		let multiplyTable = [];
-		for(var i=start;i<=end;i++){
-			for(var j=2;j<=i;j++){
+		for(let i=start;i<=end;i++){
+			for(let j=2;j<=i;j++){
 				multiplyTable.push(i+'x'+j+'='+(i*j))
 			}
 		}
